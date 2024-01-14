794a91d0b824ef3199b662ea4f312c3ba77ec014
Douglas Hill <hello@douglashill.co>
2024-01-14 10:26:38 +0000

    Add disabled ENABLE_PERFORMANCE_LOGGING

--- Package.swift
+++ Package.swift
@@ -18,6 +18,9 @@ let package = Package(
 			, dependencies: [.product(name: "Markdown", package: "swift-markdown")]
 			, path: ""
 			, exclude: ["Content", "Output"]
+//			, swiftSettings: [
+//				.define("ENABLE_PERFORMANCE_LOGGING")
+//			]
 		),
 	]
 )
