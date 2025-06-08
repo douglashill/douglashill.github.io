b5b3b3b142662defe027099b290619a9bd7a9a91
Douglas Hill <hello@douglashill.co>
2025-06-08 12:02:57 +0100

    Use cached executable if available

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
-swift run
+GENERATE_PATH="$(swift build --show-bin-path)/generate"
+
+if [ ! -f "$GENERATE_PATH" ]; then
+    echo "Cached executable not found. Building it."
+    swift build
+else
+    echo "Using cached executable."
+fi
+
+"$GENERATE_PATH"
